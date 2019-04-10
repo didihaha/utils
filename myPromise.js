253c4adeb7631533d2881f6628d8e35b29ac293e
didihaha <158673384@qq.com>
2019-04-10 16:40:15 +0800

    promise基础功能

--- myPromise.js
+++ myPromise.js
@@ -22,6 +22,8 @@ function doResolve (fn, _this) {
             reject(err)
         })
     } catch (err) {
+        if (done) return null
+        done = true
         reject(_this, err)
     }
 }
@@ -39,6 +41,8 @@ function resolve (_this, value) {
 
 function reject (_this, value) {
     _this._value = value
+    _this._state = 2
+    finale(_this)
 }
 
 function finale (_this) {
@@ -50,16 +54,54 @@ function finale (_this) {
 }
 
 function handle (_this, deferred) {
+    // 状态未改变时，将then中传入的函数缓存
     if (_this._state === 0) {
         _this._deferreds.push(deferred)
         return null
     }
+    // 已处理的标识
     _this._handled = true
+    // 放置到微栈队列处理事务
     Promise._immediateFn(function () {
         const cb = _this._state === 1 ? deferred.onFulfilled : deferred.onRejected
+        // then中若未传入任何参数直接使用resolve或reject方法处理
         if (cb === null) {
             ;(_this._state === 1 ? resolve : reject)(deferred.promise, _this._value)
             return null
         }
+        let res = null
+        try {
+            res = cb(_this._value)
+        } catch (err) {
+            reject(deferred.promise, err)
+            return null
+        }
+        resolve(deferred.promise, res)
     })
+}
+
+Promise._immediateFn =
+    (typeof setImmediate === 'function' &&
+    function(fn) {
+        setImmediate(fn);
+    }) ||
+    function(fn) {
+        setTimeoutFunc(fn, 0);
+    }
+
+Promise.prototype.then = function (onFulfilled, onRejected) {
+    const prom = new this.constructor(function () {})
+
+    handle(this, new Handler(onFulfilled, onRejected, prom))
+    return prom
+}
+
+Promise.prototype.catch = function (onRejected) {
+    return this.then(null, onRejected)
+}
+
+function Handler (onFulfilled, onRejected, promise) {
+    this.onFulfilled = onFulfilled
+    this.onRejected = onRejected
+    this.promise = promise
 }
